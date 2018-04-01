b79c6d502e3f80e76721876ec0c6f2aa32051918
Unknown <79196670853@ya.ru>
2018-04-01 16:27:05 +0300

    Make start page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
-get "store/index"
+get 'site_visitors/index'
+root 'site_visitors#index'
 resources :site_visitors
  
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
